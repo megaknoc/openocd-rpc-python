122a90cf0b35ccfe1ad5dd7e4aa3b4d361116fc4
knoc <knoc@hannover.ccc.de>
2014-02-27 17:59:09 +0100

    switched from telnetlib to raw sockets. +minor changes.

--- ocd_rpc_example.py
+++ ocd_rpc_example.py
@@ -23,7 +23,7 @@ memory (after): ['0x00000001', '0x00000000', '0xaaaaaaaa', '0x00000023',
 '0x00000042', '0x0000ffff']
 
 """
-from telnetlib import Telnet
+import socket
 
 def strToHex(data):
     return map(strToHex, data) if isinstance(data, list) else int(data, 16)
@@ -45,68 +45,71 @@ class OpenOcd:
     def __init__(self, verbose=False):
         #verbose = True
         self.verbose = verbose
-        self.telnet = Telnet()
+        self.tclRpcIp       = "127.0.0.1"
+        self.tclRpcPort     = 6666
+        self.bufferSize     = 4096
+        self.COMMAND_TOKEN  = 0x1a
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def __enter__(self):
-        self.telnet.open("127.0.0.1", 6666)
+        self.sock.connect((self.tclRpcIp, self.tclRpcPort))
         return self
 
     def __exit__(self, type, value, traceback):
-        self.send("exit")
-        self.telnet.close()
+        try:
+            self.send("exit")
+        finally:
+            self.sock.close()
 
     def send(self, cmd):
-        """Send a command string to TCL RPC."""
-        msg = (cmd + "\x1a\n").encode("ascii")
+        """Send a command string to TCL RPC. Return the result that was read."""
+        data = (cmd + "\x1a").encode("ascii")
         if self.verbose:
-            print("sent:", msg)
+            print("<- ", data)
 
-        self.telnet.write(msg)
+        self.sock.send(data)
+        return self._recv()
 
-    def sendAndRecv(self, msg):
-        """Send a command and return the received result as string."""
-        self.send(msg)
-        return self.recv()
+    def _recv(self):
+        """Read from the stream until the token (\x1a) was received."""
+        data = bytes()
+        while True:
+            chunk = self.sock.recv(self.bufferSize)
+            data += chunk
+            if bytes(self.TOKEN, encoding="ascii") in chunk:
+                break
 
-    def recv(self, until=TOKEN):
-        """Read and return the result of a read as a string."""
-        data = self.telnet.read_until(until.encode("ascii"), 3)
         if self.verbose:
-            print("recv:", data)
+            print("-> ", data)
 
         data = data.decode("ascii").strip()
-        if until == self.TOKEN:
-            data = data[:-2] # strip trailing \n\x1a
+        data = data[:-2] # strip trailing \x1a\n
 
         return data
 
     def readVariable(self, address):
-        self.telnet.read_very_eager() # flushes remaining input
-        raw = self.sendAndRecv("ocd_mdw 0x%x" % address).split(": ")
+        raw = self.send("ocd_mdw 0x%x" % address).split(": ")
         return None if (len(raw) < 2) else strToHex(raw[1])
 
     def readMemory(self, wordLen, address, n):
-        self.telnet.read_very_eager() # flushes remaining input
         self.send("array unset output") # better to clear the array before
         self.send("mem2array output %d 0x%x %d" % (wordLen, address, n))
 
-        self.send("ocd_echo $output")
-        # NOTE: is this a bug?
-        # when using 'echo $arrayName 'there is no \x1a printed at the end of
-        # the line so we must parse only up to '\n' (not to '\n\x1a')
-        output = self.recv("\n").split(" ")
+        output = self.send("ocd_echo $output").split(" ")
+
         return [int(output[2*i+1]) for i in range(len(output)//2)]
 
     def writeVariable(self, address, value):
-        ok = self.sendAndRecv("ocd_mww 0x%x 0x%x" % (address, value))
+        assert value is not None
+        self.send("mww 0x%x 0x%x" % (address, value))
 
     def writeMemory(self, wordLen, address, n, data):
         array = " ".join(["%d 0x%x" % (a, b) for a, b in enumerate(data)])
-        #print("ARRAY: ", array)
+
         self.send("array unset myArray") # better to clear the array before
         self.send("array set myArray { %s }" % array)
         self.send("array2mem myArray 0x%x %s %d" % (wordLen, address, n))
-
 
 if __name__ == "__main__":
 
@@ -114,11 +117,11 @@ if __name__ == "__main__":
         print(*args, end="\n\n")
 
     with OpenOcd() as ocd:
-        ocd.sendAndRecv("reset")
+        ocd.send("reset")
 
-        show(ocd.sendAndRecv("ocd_echo \"echo says hi!\""))
+        show(ocd.send("ocd_echo \"echo says hi!\""))
 
-        show(ocd.sendAndRecv("capture \"ocd_halt\""))
+        show(ocd.send("capture \"ocd_halt\""))
 
         addr = 0
         value = ocd.readVariable(addr)
